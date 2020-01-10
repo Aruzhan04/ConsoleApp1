b51fc309025543e89c28da31aae365525f5a6d6e
Ашкенова Аружан Азаматовна <@corp.itstep>
2020-01-10 17:20:14 +0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+           
+            
+            Console.Write("1 число: ");
+          
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("умножения: "); 
+            Console.WriteLine(a*a);
+
+            Console.Write("деление: ");
+            Console.WriteLine(a / a);
+
+
+            Console.Write("2 число: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("умножения: ");
+            Console.WriteLine(b * b);
+
+            Console.Write("деление: ");
+            Console.WriteLine(b / b);
+
+            Console.ReadKey();
+        }
+    }
+}
